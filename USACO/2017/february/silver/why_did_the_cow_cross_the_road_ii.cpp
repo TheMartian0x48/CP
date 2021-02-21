b0bbf2e9e601f8f81b50cb5cc997be50b57753cb
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2021-02-21 21:49:37 +0530

    usaco 2017 february silver why did cow cross the road II

---
+++ USACO/2017/february/silver/why_did_the_cow_cross_the_road_ii.cpp
@@ -0,0 +1,60 @@
+/*
+ * TheMartian0x48 
+ * USACO 2017 February Contest, Silver
+ * Problem 2. Why Did the Cow Cross the Road II
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define per(i, a, b) for (int i = a; i >= (b); --i)
+#define trav(a, x) for (auto &a : x)
+#define all(x) x.begin(), x.end()
+#define rll(x) x.rbegin(), x.rend()
+#define sz(x) (int)x.size()
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<long long>;
+// (* math *)
+ll eea(ll a, ll b, ll &x, ll &y) {if (b == 0){x = 1,y = 0;return a;}
+ll x1, y1;  ll g = eea(b, a % b, x1, y1);x = y1;y = x1 - y1 * (a / b);return g;}
+// (* read and write *)
+template <class T>
+void re(vector<T> &v, int n) {v.resize(n); for (auto &e : v) cin >> e;}
+template <class T> 
+void re(vector<T> &v){for (auto &e : v) cin >> e;}
+template <class T>
+void pr(vector<T> &v, char sep = ' ') {for (auto e : v) cout << e << sep;}
+// clang-format on
+
+// ---------------- (* Extract here *)
+// ----------------
+
+
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  freopen("maxcross.in", "r", stdin);
+  freopen("maxcross.out", "w", stdout);
+  int n, k, b;
+  cin >> n >> k >> b;
+  vi v(n+1, 1);
+  while (b--) {
+    cin >> v[0];
+    v[v[0]]--;
+  }
+  int res = n + 1, working = 0, j = 1;
+  rep(i,1,n+1) {
+    working += v[i];
+    if (i - j + 1 < k) continue;
+    res = min(res, k - working);
+    working -= v[j++];
+  }
+  cout << res;
+}
